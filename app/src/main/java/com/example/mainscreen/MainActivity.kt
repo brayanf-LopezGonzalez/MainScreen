8f5635e48a5bdab7a63b17e45a04a2fbce7e7e46
Brayan Federico Lopez Gonzalez <brayanflg99@gmail.com>
2026-03-04 20:16:50 -0700

    Inicio de pantalla Login

--- app/src/main/java/com/example/mainscreen/MainActivity.kt
+++ app/src/main/java/com/example/mainscreen/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -15,6 +16,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,10 +29,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mainscreen.ui.theme.MainScreenTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,22 +46,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MainScreenTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    /*Greeting(
+                /*Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )*/
+                    )
                     MainScreen(modifier = Modifier.padding(innerPadding))
+                }*/
+
+                val navController = rememberNavController()
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "main",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+
+                        composable("main") {
+                            MainScreen(navController)
+                        }
+
+                        composable("login") {
+                            LoginScreen()
+                        }
+                    }
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
+fun MainScreen(navController: NavController) {
     Box(
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
             .background(Color.White)//box to fill all the screen, with wihte color
     ) {
@@ -79,7 +110,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 8.dp),
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                textAlign = TextAlign.Center
             )
 
             Text(
@@ -89,17 +120,18 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 32.dp),
-                textAlign = androidx.compose.ui.text.style.TextAlign.Center
+                textAlign = TextAlign.Center
             )
             Spacer(modifier = Modifier.weight(1f))
 
-            Box(
+            Button(
+                onClick = {navController.navigate("login")},
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp)
                     .clip(RoundedCornerShape(8.dp))
                     .background(Color.Blue),
-                contentAlignment = Alignment.Center
+                shape = RoundedCornerShape(8.dp)
             ) {
                 Text(
                     text = "Login",
@@ -111,22 +143,20 @@ fun MainScreen(modifier: Modifier = Modifier) {
 
             Spacer(modifier = Modifier.height(12.dp))
 
-            Box(
+            OutlinedButton(
+                onClick = { },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(50.dp)
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(Color.White)
-                    .border(
-                        width = 1.dp,
-                        color = Color.Blue,
-                        shape = RoundedCornerShape(8.dp)
-                    ),
-                contentAlignment = Alignment.Center
+                    .height(50.dp),
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(1.dp, Color.Blue),
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = Color.Blue,
+                    containerColor = Color.White
+                )
             ) {
                 Text(
                     text = "Sign up",
-                    color = Color.Blue,
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Medium
                 )
@@ -135,10 +165,56 @@ fun MainScreen(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(28.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(modifier = Modifier.height(150.dp))
+
+            Text(
+                text = "Hello!",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,//font and type
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                textAlign = TextAlign.Center
+            )
+
+
+
+            Text(
+                text = "Hello!",
+                fontSize = 50.sp,
+                fontWeight = FontWeight.Bold,//font and type
+                color = Color.Black,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                textAlign = TextAlign.Center
+            )
+
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun MainScreenPreview() {
     MainScreenTheme {
-        MainScreen()
+        val navController = rememberNavController()
+        MainScreen(navController)
     }
 }
